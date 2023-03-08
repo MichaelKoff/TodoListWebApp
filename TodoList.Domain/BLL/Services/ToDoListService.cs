6b63c41b308816e8a7ed9ceab1cdd4bdcfec3b3f
Mikhail Kozhevnikov <michael.koff019@gmail.com>
2023-03-08 22:18:50 +0100

    added implementation for TodoListService and TodoListTaskService

---
+++ TodoList.Domain/BLL/Services/ToDoListService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.BLL.Interfaces;
+using TodoList.Domain.DAL.Entities;
+using TodoList.Domain.DAL.Interfaces;
+
+namespace TodoList.Domain.BLL.Services
+{
+    public class ToDoListService : IToDoListService
+    {
+        private readonly IRepository<ToDoList> _repository;
+
+        public ToDoListService(IRepository<ToDoList> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AddAsync(ToDoList todoList)
+        {
+            await _repository.AddAsync(todoList);
+        }
+
+        public async Task DeleteAsync(ToDoList todoList)
+        {
+            await _repository.DeleteAsync(todoList);
+        }
+
+        public async Task<List<ToDoList>> GetAllAsync(string userId)
+        {
+            return await _repository.GetAllAsync(userId);
+        }
+
+        public async Task<ToDoList> GetByIdAsync(int id, string userId)
+        {
+            return await _repository.GetByIdAsync(id, userId);
+        }
+
+        public async Task UpdateAsync(ToDoList todoList)
+        {
+            await _repository.UpdateAsync(todoList);
+        }
+    }
+}
---
+++ TodoList.Domain/BLL/Services/ToDoListTaskService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.BLL.Interfaces;
+using TodoList.Domain.DAL.Entities;
+using TodoList.Domain.DAL.Interfaces;
+
+namespace TodoList.Domain.BLL.Services
+{
+    public class ToDoListTaskService : IToDoListTaskService
+    {
+        private readonly IRepository<ToDoListTask> _repository;
+
+        public ToDoListTaskService(IRepository<ToDoListTask> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AddAsync(ToDoListTask todoListTask)
+        {
+            await _repository.AddAsync(todoListTask);
+        }
+
+        public async Task DeleteAsync(ToDoListTask todoListTask)
+        {
+            await _repository.DeleteAsync(todoListTask);
+        }
+
+        public async Task<List<ToDoListTask>> GetAllAsync(string userId)
+        {
+            return await _repository.GetAllAsync(userId);
+        }
+
+        public async Task<ToDoListTask> GetByIdAsync(int id, string userId)
+        {
+            return await _repository.GetByIdAsync(id, userId);
+        }
+
+        public async Task UpdateAsync(ToDoListTask todoListTask)
+        {
+            await _repository.UpdateAsync(todoListTask);
+        }
+    }
+}
