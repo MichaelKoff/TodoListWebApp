a68892169f8b3a0d8769a0e40504d5daa4a54b53
Mikhail Kozhevnikov <michael.koff019@gmail.com>
2023-03-08 22:16:30 +0100

    added IRepository interface

---
+++ TodoList.Domain/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.DAL.Entities;
+
+namespace TodoList.Domain.DAL.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id, string userId);
+        Task<List<T>> GetAllAsync(string userId);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
