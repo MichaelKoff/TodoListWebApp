38f66231cb71509dad7b49089545fa100ea705c8
Mikhail Kozhevnikov <michael.koff019@gmail.com>
2023-03-08 22:18:21 +0100

    added interfaces for TodoListService and TodoListTaskService

---
+++ TodoList.Domain/BLL/Interfaces/IToDoListService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.DAL.Entities;
+
+namespace TodoList.Domain.BLL.Interfaces
+{
+    public interface IToDoListService
+    {
+        Task<ToDoList> GetByIdAsync(int id, string userId);
+        Task<List<ToDoList>> GetAllAsync(string userId);
+        Task AddAsync(ToDoList todoList);
+        Task UpdateAsync(ToDoList todoList);
+        Task DeleteAsync(ToDoList todoList);
+    }
+}
---
+++ TodoList.Domain/BLL/Interfaces/IToDoListTaskService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.DAL.Entities;
+
+namespace TodoList.Domain.BLL.Interfaces
+{
+    public interface IToDoListTaskService
+    {
+        Task<ToDoListTask> GetByIdAsync(int id, string userId);
+        Task<List<ToDoListTask>> GetAllAsync(string userId);
+        Task AddAsync(ToDoListTask todoListTask);
+        Task UpdateAsync(ToDoListTask todoListTask);
+        Task DeleteAsync(ToDoListTask todoListTask);
+    }
+}
