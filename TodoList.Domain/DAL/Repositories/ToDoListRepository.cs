745a071afe3ce979d02b9ec535562413560319db
Mikhail Kozhevnikov <michael.koff019@gmail.com>
2023-03-08 22:17:06 +0100

    implemented repositories for TodoList and TodoListTask

---
+++ TodoList.Domain/DAL/Repositories/ToDoListRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Domain.DAL.Entities;
+using TodoList.Domain.DAL.Interfaces;
+
+namespace TodoList.Domain.DAL.Repositories
+{
+    public class ToDoListRepository : IRepository<ToDoList>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoListRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ToDoList todoList)
+        {
+            await _context.ToDoLists.AddAsync(todoList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ToDoList todoList)
+        {
+            _context.ToDoLists.Remove(todoList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ToDoList>> GetAllAsync(string userId)
+        {
+            return await _context.ToDoLists
+                .Include(tl => tl.ToDoListTasks)
+                .Where(tl => tl.ApplicationUserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<ToDoList> GetByIdAsync(int id, string userId)
+        {
+            return await _context.ToDoLists
+                .Include(tl => tl.ToDoListTasks)
+                .FirstOrDefaultAsync(tl => tl.ApplicationUserId == userId && tl.Id == id);
+        }
+
+        public async Task UpdateAsync(ToDoList todoList)
+        {
+            _context.Entry(todoList).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TodoList.Domain/DAL/Repositories/ToDoListTaskRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.DAL.Entities;
+using TodoList.Domain.DAL.Interfaces;
+
+namespace TodoList.Domain.DAL.Repositories
+{
+    public class ToDoListTaskRepository : IRepository<ToDoListTask>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoListTaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ToDoListTask todoListTask)
+        {
+            await _context.AddAsync(todoListTask);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ToDoListTask todoListTask)
+        {
+            _context.ToDoListTasks.Remove(todoListTask);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ToDoListTask>> GetAllAsync(string userId)
+        {
+            return await _context.ToDoListTasks
+                .Where(t => t.ToDoList.ApplicationUserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<ToDoListTask> GetByIdAsync(int id, string userId)
+        {
+            return await _context.ToDoListTasks.FirstOrDefaultAsync(t => t.Id == id && t.ToDoList.ApplicationUserId == userId);
+        }
+
+        public async Task UpdateAsync(ToDoListTask todoListTask)
+        {
+            _context.Entry(todoListTask).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
