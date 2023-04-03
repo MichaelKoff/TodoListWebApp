de05a09961481dc74864da35d665ab861b2e4bb5
MichaelKoff <michael.koff019@gmail.com>
2023-04-03 17:10:25 +0200

    added CombinedTodoListViewModel

---
+++ TodoList.MVC/Models/CombinedTodoListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Domain.DAL.Entities;
+using TodoList.MVC.ViewModels;
+
+namespace TodoList.MVC.Models
+{
+    public class CombinedTodoListViewModel
+    {
+        public IEnumerable<ToDoListViewModel> TodoLists { get; set; }
+        public ToDoListViewModel SelectedTodoList { get; set; }
+    }
+}
