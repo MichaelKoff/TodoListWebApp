23e519c185f429e3451883791dbe2d3abb658b0a
MichaelKoff <michael.koff019@gmail.com>
2023-04-03 16:58:01 +0200

    added Mapping profile

---
+++ TodoList.MVC/Mappers/MappingProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using TodoList.Domain.DAL.Entities;
+using TodoList.MVC.ViewModels;
+
+namespace TodoList.MVC.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ToDoList, ToDoListViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.ToDoListTasks, opt => opt.MapFrom(src => src.ToDoListTasks));
+
+            CreateMap<ToDoListViewModel, ToDoList>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.ToDoListTasks, opt => opt.MapFrom(src => src.ToDoListTasks));
+
+            CreateMap<ToDoListTask, ToDoListTaskViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ToDoListId, opt => opt.MapFrom(src => src.ToDoListId))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.DueDate, opt => opt.MapFrom(src => src.DueDate))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreationDate))
+                .ForMember(dest => dest.Reminder, opt => opt.MapFrom(src => src.Reminder));
+
+            CreateMap<ToDoListTaskViewModel, ToDoListTask>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ToDoListId, opt => opt.MapFrom(src => src.ToDoListId))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.DueDate, opt => opt.MapFrom(src => src.DueDate))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => src.CreationDate))
+                .ForMember(dest => dest.Reminder, opt => opt.MapFrom(src => src.Reminder));
+        }
+    }
+}
