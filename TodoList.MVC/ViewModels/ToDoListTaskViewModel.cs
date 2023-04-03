6a356d356e5773e32731802ece70679f866da23d
MichaelKoff <michael.koff019@gmail.com>
2023-04-03 16:59:05 +0200

    added view models

---
+++ TodoList.MVC/ViewModels/ToDoListTaskViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using TodoList.Domain.DAL.Entities;
+using TodoList.Domain.DAL.Enums;
+
+namespace TodoList.MVC.ViewModels
+{
+    public class ToDoListTaskViewModel : BaseEntity
+    {
+        private string _title;
+        public int ToDoListId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"^\s*\S+(?:\s+\S+)*\s*$", ErrorMessage = "Title cannot be empty or whitespace.")]
+        public string Title
+        {
+            get => _title;
+            set => _title = Regex.Replace(value.Trim(), @"\s+", " ");
+        }
+
+        [MaxLength(255)]
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public TodoStatus Status { get; set; } = TodoStatus.NotStarted;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime? Reminder { get; set; }
+    }
+}
---
+++ TodoList.MVC/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using TodoList.Domain.DAL.Entities;
+
+namespace TodoList.MVC.ViewModels
+{
+    public class ToDoListViewModel : BaseEntity
+    {
+        private string _title;
+
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"^\s*\S+(?:\s+\S+)*\s*$", ErrorMessage = "Title cannot be empty or whitespace.")]
+        public string Title 
+        {
+            get => _title;
+            set => _title = Regex.Replace(value.Trim(), @"\s+", " ");
+        }
+
+        public List<ToDoListTaskViewModel> ToDoListTasks { get; set; }
+
+    }
+}
