206ef61dcde4fabed6cab1877eb94fbc84b1d76e
MichaelKoff <michael.koff019@gmail.com>
2023-04-07 19:47:48 +0200

    Updated form for creating tasks, moved script into separate file, added css rule for asp-validation

---
+++ TodoList.MVC/wwwroot/js/create-todo-task.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(document).off("submit", "#create-task").on("submit", "#create-task", function (e) {
+        e.preventDefault();
+        let form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            method: "POST",
+            data: form.serialize(),
+            success: function (data) {
+                let taskListHtml = $(data).find("#task-list").html();
+                let formHtml = $(data).find("#create-task").html();
+                $("#task-list-container #task-list").html(taskListHtml);
+                $("#task-list-container #create-task").html(formHtml);
+
+                form.find('input[name="Title"]').val('');
+                form.find('input[name="Title"]').focus();
+            },
+            error: function () {
+                console.log("Error creating todo list.");
+            }
+        });
+    });
+});
